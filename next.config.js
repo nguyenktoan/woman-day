5509e7539e6054949cc61beef3cbbbbf11f0e4d8
nguyenktoan <147793538+nguyenktoan@users.noreply.github.com>
2025-03-08 23:00:25 +0700

    Mô tả thay đổi

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export", // Xuất thành static site
-  basePath: "/woman-day", // Đường dẫn tương ứng với GitHub Pages repo
   images: {
-    unoptimized: true, // Hỗ trợ ảnh khi xuất static site
     domains: [
       "images.unsplash.com",
       "www.transparenttextures.com",
